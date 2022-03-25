7501b3b86182cfb89f3fe5851fa29218875a106b
youngbo9i <ybmj58106159131>
2022-03-25 15:43:50 +0800

    update

--- code/README.md
+++ code/README.md
@@ -1,10 +1,10 @@
-#Predicting novel drug-disease associations for drug repositioning through variational infer-ence and graph autoencoder
+# Predicting novel drug-disease associations for drug repositioning through variational infer-ence and graph autoencoder
 Code for our paper“”
 ## DataSets
 * data/drug_dis.csv is the drug_disease association matrix, which contain 18416 associations between 269 drugs and 598 diseases.
 * data/drug_sim.csv is the drug similarity matrix of 269 diseases,which is calculated based on drug target features.
 * data/dis_sim.csv is the disease similarity matrix of 598 diseases,which is calculated based on disease mesh descriptors.
-##Requirements
+## Requirements
 The code has been tested running under Python 3.8.0, with the following packages and their dependencies installed:
 ###
     numpy==1.21.5
--- code/models.py
+++ code/models.py
@@ -26,8 +26,6 @@ class AE(nn.Module):
     def __init__(self,feat_dim,hid_dim,out_dim,bias=False):
         super(AE,self).__init__()
         self.conv1 = GraphConv(feat_dim,hid_dim,bias=bias,activation=F.relu)
-        #自己加的
-        # self.conv5=GraphConv(hid_dim,hid_dim,bias=bias,activation=torch.sigmoid)
         self.mu = GraphConv(hid_dim,out_dim,bias=bias,activation=torch.sigmoid)
         self.conv3 = GraphConv(out_dim,hid_dim,bias=bias,activation=F.relu)
         self.conv4 = GraphConv(hid_dim,feat_dim,bias=bias,activation=torch.sigmoid)
@@ -35,8 +33,6 @@ class AE(nn.Module):
 
     def encoder(self,g,x):
         x = self.conv1(g,x)
-        #我加的
-        # x=self.conv5(g,x)
         h = self.mu(g,x)
         std = self.logvar(g,x)
         return h,std
@@ -50,7 +46,6 @@ class AE(nn.Module):
         if self.training:
             std = torch.exp(0.5 * logvar)
             eps = torch.randn_like(std)
-            #这里实际上就是通过标准差和均值得到z
             return eps.mul(std).add_(mu)
         else:
             return mu
@@ -64,44 +59,9 @@ class LP(nn.Module):
     def __init__(self,hid_dim,out_dim,bias=False):
         super(LP,self).__init__()
         self.res1 = GraphConv(out_dim,hid_dim,bias=bias,activation=F.relu)
-        # self.res2 = GraphConv(hid_dim,hid_dim,bias=bias,activation=torch.tanh)
-        # self.res3 = GraphConv(hid_dim,hid_dim,bias=bias,activation=F.relu)
         self.res2 = GraphConv(hid_dim,out_dim,bias=bias,activation=torch.sigmoid)
 
     def forward(self,g,z):
         z = self.res1(g,z)
         res = self.res2(g,z)
         return res,z
-    # def __init__(self, hid_dim, out_dim, bias=False):
-    #     super(LP, self).__init__()
-    #     self.res1 = GraphConv(out_dim, hid_dim, bias=bias, activation=F.relu)
-    #     self.res2 = GraphConv(hid_dim,128,bias=bias,activation=torch.tanh)
-    #     self.res3 = GraphConv(128,hid_dim,bias=bias,activation=F.relu)
-    #     self.res4 = GraphConv(hid_dim, out_dim, bias=bias, activation=torch.sigmoid)
-    # def forward(self,g,z):
-    #     z = self.res2(g,self.res1(g,z))
-    #     # print(z.size())
-    #     res = self.res4(g,self.res3(g,z))
-    #     return res,z
-
-# class GNNq(nn.Module):
-#     def __init__(self,drug_sim,dis_sim,args):
-#         super(GNNq,self).__init__()
-#         self.gnnq_drug = AE(drug_sim.shape[1], 256, args.hidden)
-#         self.gnnq_dis = AE(dis_sim.shape[0], 256, args.hidden)
-#
-#     def forward(self, x_drug0, x_dis0,g_drug,g_dis):
-#         h_drug,std_drug,x_drug = self.gnnq_drug(g_drug, x_drug0)
-#         h_dis,std_dis,x_dis = self.gnnq_dis(g_dis, x_dis0)
-#         return h_drug,std_drug,x_drug,h_dis,std_dis,x_dis
-#
-# class GNNp(nn.Module):
-#     def __init__(self,drug_dis,args):
-#         super(GNNp,self).__init__()
-#         self.gnnp_drug = LP(args.hidden, drug_dis.shape[1])
-#         self.gnnp_dis = LP(args.hidden, drug_dis.shape[0])
-#
-#     def forward(self,y0,g_drug,g_dis):
-#         y_drug,z_drug = self.gnnp_drug(g_drug, y0)
-#         y_dis,z_dis = self.gnnp_dis(g_dis, y0.t())
-#         return y_drug,z_drug,y_dis,z_dis
